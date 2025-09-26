70ea46cae88e992c94197068cb043699603aca2e
Moua Kelvin <826665@stma.k12.mn.us>
2025-09-26 14:10:48 -0500

    update-AnimalSurvey.py

--- Assignment/AnimalSurvey.py
+++ Assignment/AnimalSurvey.py
@@ -3,6 +3,8 @@ Why = input("Why do you like this animal?\n>")
 Where_is_it_from = input("Where is this animal from?\n>")
 Fact = input("Whats a fun fact about this animal?\n>")
 Color = input("What are some colors that are on your animal?\n>")
+Sounds = input("What noises does this animal make?\n>")
+Land_Or_Underwater = input("Is your animal on land or underwater or both?\n")
 
 
 print("_____Animal Survey_____")
@@ -10,8 +12,11 @@ print("_____Animal Survey_____")
 print("Favorite animal: " + Favortie_Animal)
 print("Why?: " + Why)
 print("Where is it from?: " + Where_is_it_from)
-print("You Animal Facts: " + Fact)
-print("You Animals Colors: " + Color)
+print("Your Animal Facts: " + Fact)
+print("Your Animals Colors: " + Color)
+print("The sound it makes is: " + Sounds)
+print("Your animal is on: " + Land_Or_Underwater)
+
 
 input("How would you rate this survey\n>")
 print("Thanks for taking this survey.")
