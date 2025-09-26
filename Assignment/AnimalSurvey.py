ed0d7fa9c8b442abbb6487bcd3511d5e6d8fb5ba
Moua Kelvin <826665@stma.k12.mn.us>
2025-09-26 13:54:10 -0500

    AnimalSurvey.py

---
+++ Assignment/AnimalSurvey.py
@@ -0,0 +1,17 @@
+Favortie_Animal = input("What is your favorite animal?\n>")
+Why = input("Why do you like this animal?\n>")
+Where_is_it_from = input("Where is this animal from?\n>")
+Fact = input("Whats a fun fact about this animal?\n>")
+Color = input("What are some colors that are on your animal?\n>")
+
+
+print("_____Animal Survey_____")
+
+print("Favorite animal: " + Favortie_Animal)
+print("Why?: " + Why)
+print("Where is it from?: " + Where_is_it_from)
+print("You Animal Facts: " + Fact)
+print("You Animals Colors: " + Color)
+
+input("How would you rate this survey\n>")
+print("Thanks for taking this survey.")
