f61b23614e81fc9cdf20847cc013019d887bc1b4
Moua Kelvin <826665@stma.k12.mn.us>
2025-10-09 13:45:30 -0500

    SimpleQuiz.py

---
+++ Assignment/SimpleQuiz.py
@@ -0,0 +1,29 @@
+answer_one = input ("What is 2+2?\n>")
+answer_two = input ("What New Game is releasing that starts with An \n>")
+answer_three = input ("What the new slient hill game?\n>")
+answer_four = input ("Whats the new battle field game that is first person\n>")
+answer_five = input ("What is 5 * 5?\n>")
+
+def SimpleQuiz():
+    score = 0
+
+if answer_one.lower() == "4":
+    score = + 1
+    score = score + 1
+
+
+if answer_two.lower() == "ananta":
+    score = score + 1
+
+if answer_three.lower() == "slient hill f":
+    score == score + 1
+
+if answer_four.lower() == "battle field 6":
+    score == score + 1
+
+if answer_five.lower == 25 :
+    score == score + 1
+
+print("Score: " + str(score) + "/5")
+
+SimpleQuiz()
---
+++ Practice/3.2practice.py
@@ -0,0 +1,17 @@
+password1 = "YourBad"
+password2 = "yourbad"
+
+
+
+if password1 == password2:
+    print("Six Eyes")
+else:
+    print("Aka")
+
+
+    password3 = input("Insert the correct password\n>")
+    password = "TheHonoredOne"
+    if password == password3:
+        print("Access Granted. Welcome Strongest Of Today.")
+    else:
+        print("Access Decline. Your still weak")
